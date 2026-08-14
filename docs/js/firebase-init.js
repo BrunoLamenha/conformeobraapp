c7b831a0c4bb8206d16b9320d1261da23b90ad0b
BrunoLamenha <lamenhabruno@gmail.com>
2026-08-14 19:56:06 -0300

    Update firebase-init.js

--- docs/js/firebase-init.js
+++ docs/js/firebase-init.js
@@ -11,15 +11,7 @@
 // Enquanto isso, a app usa localStorage como fallback (funciona offline).
 // ============================================================================
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDmkwQQUsLx_Ye8DVOHObazectctArQUkA",
-  authDomain: "conformeobra-6ae78.firebaseapp.com",
-  projectId: "conformeobra-6ae78",
-  storageBucket: "conformeobra-6ae78.firebasestorage.app",
-  messagingSenderId: "299379032493",
-  appId: "1:299379032493:web:2a20e0f762af6ff6afe124",
-  measurementId: "G-YY636RKB5B"
-};
+import { firebaseConfig } from './firebase-config.local.js'
 
 // Tentar carregar config do arquivo local (se existir)
 try {
