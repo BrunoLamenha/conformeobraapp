b2a79533df2d72b2b2370b1b9cd329d66a99fd82
BrunoLamenha <lamenhabruno@gmail.com>
2026-08-14 19:55:06 -0300

    Update firebase-config.local.js

--- docs/firebase-config.local.js
+++ docs/firebase-config.local.js
@@ -1,1 +1,9 @@
-
+export const firebaseConfig = {
+  apiKey: "AIzaSyDmkwQQUsLx_Ye8DVOHObazectctArQUkA",
+  authDomain: "conformeobra-6ae78.firebaseapp.com",
+  projectId: "conformeobra-6ae78",
+  storageBucket: "conformeobra-6ae78.firebasestorage.app",
+  messagingSenderId: "299379032493",
+  appId: "1:299379032493:web:2a20e0f762af6ff6afe124",
+  measurementId: "G-YY636RKB5B"
+};
